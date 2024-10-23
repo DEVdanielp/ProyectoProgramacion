750acc628a4bfcc670ba6318932e26f7cdc9b50a
MANUELAG\manue <manuelagomezz1234chaparro@gmail.com>
2024-10-23 12:17:11 -0500

    cambios

---
+++ Hospital/Hospital.Web/Core/Extensions/QueryableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Hospital.Web.Core.Pagination;
+
+namespace Hospital.Web.Core.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> query, PaginationRequest request) 
+        {
+            return query.Skip((request.Page - 1) * request.RecordsPerPage)
+                        .Take(request.RecordsPerPage);
+        }
+
+    }
+}
